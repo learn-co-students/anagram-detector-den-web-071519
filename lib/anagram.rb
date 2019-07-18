e18beadf61b13809c5cf4adb2379d6c174ef3da7
SwanHub <jdprince555@gmail.com>
2019-07-17 20:20:11 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
-# Your code goes here!
+require 'pry'
+
+def reorder_word(word)
+  word.split("").sort.join("")
+end
+
+class Anagram
+
+  attr_accessor :match_word
+
+  def initialize(match_word)
+    @match_word = match_word
+  end
+
+  def match(array)
+
+    successful_matches = []
+    array.each {|anagram_word|
+      # binding.pry
+      if reorder_word(anagram_word) == reorder_word(@match_word)
+        successful_matches << anagram_word
+      end
+      }
+    successful_matches
+
+  end
+
+end
